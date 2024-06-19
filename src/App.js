cd6b7c844735f2e970a36e34ddb22afe22eade03
Thet Su Nwe <thetsunwe96@gmail.com>
2024-06-19 15:25:14 +0630

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to my new blog";
-  const likes = 50;
-  // const person = { name : "yoshi", age : 30}
-  const link = "http://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p> Liked: {likes} times</p>
-
-        {/* <p> {person}</p> //error */}
-        <p> { 10 } </p> {/*10*/}
-        <p> {"Hello, jojo"} </p> {/*Hello, jojo*/}
-        <p> {[1,2,3,4,5]} </p> {/*1,2,3,4,5*/}
-        <p> { Math.random() * 10} </p> {/*random number*/}
-
-        <a href={link}> Google Site</a>
-
+        <Home />
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Cookie Blog</h1>
+            <div className="link">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
